97686f7ab84b3990c978db01ea16869e1211740a
narendra_nk <kalekar.narendra@gmail.com>
2018-06-19 09:11:07 +0530

    Added cashbook tabs.

---
+++ src/app/dashboard/cashbook/cashbook-report-tab/cashbook-report-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cashbook-report-tab',
+  templateUrl: './cashbook-report-tab.component.html',
+  styleUrls: ['./cashbook-report-tab.component.css']
+})
+export class CashbookReportTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
